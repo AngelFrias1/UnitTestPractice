18ccb404f4b8f74345cafedc629607ab3238916e
Angel <afrias7@mail.csuchico.edu>
2022-04-27 22:09:29 -0700

    unit problem testing

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,3 +18,65 @@ TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
 }
+
+//Function 1
+TEST(PracticeTest, zzz_repeats)
+{
+Practice testObject;
+ASSERT_EQ(testObject.count_starting_repeats("zzz"),3);
+}
+
+//Function 2
+TEST(PracticeTest, ascending)
+{
+Practice testObject;
+int a= 1;
+int b= 2;
+int c= 3;
+testObject.sortDescending(a,b,c);
+ASSERT_EQ(a,3);
+ASSERT_EQ(b,2);
+ASSERT_EQ(c,1);
+}
+
+TEST(PracticeTest, ascending2)
+{
+Practice testObject;
+int a = 3;
+int b = 1;
+int c = 2;
+
+testObject.sortDescending(a,b,c);
+ASSERT_EQ(a,3);
+ASSERT_EQ(b,2);
+ASSERT_EQ(c,1);
+}
+TEST(PracticeTest, ascending3)
+{
+Practice testObject;
+int a = 4;
+int b = 4;
+int c = 4;
+testObject.sortDescending(a,b,c);
+ASSERT_EQ(a,4);
+ASSERT_EQ(b,4);
+ASSERT_EQ(c,4);
+}
+
+//Function 3
+TEST(PracticeTest,palindrome1)
+{
+Practice testObject;
+ASSERT_EQ(testObject.isPalindrome("level"),true);
+}
+
+TEST(PracticeTest,palindrome2)
+{
+Practice testObject;
+ASSERT_EQ(testObject.isPalindrome("foo"),false);
+}
+TEST(PracticeTest,palindrome3)
+{
+Practice testObject;
+ASSERT_EQ(testObject.isPalindrome("   "),true);
+}
